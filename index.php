2fe3fb6bf0a96efd2cfaa058eda0dc32719c2821
Popov Sergey <serpp@me.com>
2023-11-27 15:07:22 +1000

    modified composer.json composer.lock index.php src/Runner.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 
-echo 'Hexlet one love.';
-echo "\n"
+// Путь который будет использован при глобальной установке пакета
+$autoloadPath1 = __DIR__ . '/home/sergeykms/.config/composer/vendor/autoload.php';
+// Путь для локальной работы с проектом
+$autoloadPath2 = __DIR__ . '/vendor/autoload.php';
+
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+
+use HexletPhp\src\Runner;
+
+print_r(Runner\run());
---
+++ src/Runner.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace HexletPhp\src\Runner;
+
+function run()
+{
+    $collection = collect(['taylor', 'abigail', null])->map(function ($name) {
+        return strtoupper($name);
+    });
+
+    return $collection;
+}
